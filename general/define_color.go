9f19443ac1e8cf44420dd697b3161bd570ed76a8
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-07-05 09:24:51 +0800

    新增输出颜色定义

---
+++ general/define_color.go
@@ -0,0 +1,61 @@
+/*
+File: define_color.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2024-05-29 15:46:55
+
+Description: 定义输出颜色
+*/
+
+package general
+
+import "github.com/gookit/color"
+
+var (
+	FgBlackText        = color.FgBlack.Render        // 前景色 - 黑色
+	FgWhiteText        = color.FgWhite.Render        // 前景色 - 白色
+	FgLightWhiteText   = color.FgLightWhite.Render   // 前景色 - 亮白色
+	FgGrayText         = color.FgGray.Render         // 前景色 - 灰色
+	FgRedText          = color.FgRed.Render          // 前景色 - 红色
+	FgLightRedText     = color.FgLightRed.Render     // 前景色 - 亮红色
+	FgGreenText        = color.FgGreen.Render        // 前景色 - 绿色
+	FgLightGreenText   = color.FgLightGreen.Render   // 前景色 - 亮绿色
+	FgYellowText       = color.FgYellow.Render       // 前景色 - 黄色
+	FgLightYellowText  = color.FgLightYellow.Render  // 前景色 - 亮黄色
+	FgBlueText         = color.FgBlue.Render         // 前景色 - 蓝色
+	FgLightBlueText    = color.FgLightBlue.Render    // 前景色 - 亮蓝色
+	FgMagentaText      = color.FgMagenta.Render      // 前景色 - 品红
+	FgLightMagentaText = color.FgLightMagenta.Render // 前景色 - 亮品红
+	FgCyanText         = color.FgCyan.Render         // 前景色 - 青色
+	FgLightCyanText    = color.FgLightCyan.Render    // 前景色 - 亮青色
+
+	BgBlackText        = color.BgBlack.Render        // 背景色 - 黑色
+	BgWhiteText        = color.BgWhite.Render        // 背景色 - 白色
+	BgLightWhiteText   = color.BgLightWhite.Render   // 背景色 - 亮白色
+	BgGrayText         = color.BgGray.Render         // 背景色 - 灰色
+	BgRedText          = color.BgRed.Render          // 背景色 - 红色
+	BgLightRedText     = color.BgLightRed.Render     // 背景色 - 亮红色
+	BgGreenText        = color.BgGreen.Render        // 背景色 - 绿色
+	BgLightGreenText   = color.BgLightGreen.Render   // 背景色 - 亮绿色
+	BgYellowText       = color.BgYellow.Render       // 背景色 - 黄色
+	BgLightYellowText  = color.BgLightYellow.Render  // 背景色 - 亮黄色
+	BgBlueText         = color.BgBlue.Render         // 背景色 - 蓝色
+	BgLightBlueText    = color.BgLightBlue.Render    // 背景色 - 亮蓝色
+	BgMagentaText      = color.BgMagenta.Render      // 背景色 - 品红
+	BgLightMagentaText = color.BgLightMagenta.Render // 背景色 - 亮品红
+	BgCyanText         = color.BgCyan.Render         // 背景色 - 青色
+	BgLightCyanText    = color.BgLightCyan.Render    // 背景色 - 亮青色
+
+	InfoText      = color.Info.Render      // Info 文本
+	NoteText      = color.Note.Render      // Note 文本
+	LightText     = color.Light.Render     // Light 文本
+	ErrorText     = color.Error.Render     // Error 文本
+	DangerText    = color.Danger.Render    // Danger 文本
+	NoticeText    = color.Notice.Render    // Notice 文本
+	SuccessText   = color.Success.Render   // Success 文本
+	CommentText   = color.Comment.Render   // Comment 文本
+	PrimaryText   = color.Primary.Render   // Primary 文本
+	WarnText      = color.Warn.Render      // Warn 文本
+	QuestionText  = color.Question.Render  // Question 文本
+	SecondaryText = color.Secondary.Render // Secondary 文本
+)
